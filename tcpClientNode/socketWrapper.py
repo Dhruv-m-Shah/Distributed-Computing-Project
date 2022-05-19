cf16ed15e12e83f8f07d5677f135b678efd40cac
Dhruv-m-Shah <shahdhruv2001@gmail.com>
2022-05-19 19:57:51 -0400

    Added socket wrapper

--- tcpClientNode/socketWrapper.py
+++ tcpClientNode/socketWrapper.py
@@ -1,10 +1,14 @@
 import socket, ssl, pprint
 import time
 import json
+import sys
 import constants
 
 class SocketWrapper:
-    def __init__(self, url, port, blocking):
+    def __init__(self, url, port, blocking, max_retries = 3):
+        self.max_retries = max_retries
+        self.url = url
+        self.port = port
         try:
             self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error as e:
@@ -13,15 +17,20 @@ class SocketWrapper:
         self.ssl_sock = ssl.wrap_socket(self.soc,
                            ca_certs="../certs/ca/ca.crt",
                            cert_reqs=ssl.CERT_REQUIRED)
-        try:
-            ret = self.ssl_sock.connect_ex((url, port))
-        except socket.error as e:
-            print("Connection error: " + str(e))
+        ret = self.ssl_sock.connect_ex((url, port))
+        if(ret != 0):
+            print("Connection error: " + str(ret))
             sys.exit(1)
-        self.ssl_sock.setblocking(0) # Make sockets async
+        self.ssl_sock.setblocking(blocking) # Make sockets async
     
     def connect(self, url, port):
-        self.ssl_sock.connect((url, port))
+        ret = 0
+        for i in range(self.max_retries):
+            ret = self.ssl_sock.connect((url, 8001))
+            if(ret == 0):
+                return
+        print("Connection error: " + str(ret))
+        sys.exit(1)
 
     def recv(self, byteSize):
         data = self.ssl_sock.recv(constants.SOCK_RECV_SIZE_IN_BYTES)
